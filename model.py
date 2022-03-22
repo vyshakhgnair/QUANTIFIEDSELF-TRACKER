337b0f98ab876a4d4c81456a387a42cd79daeeb0
vyshakhgnair <vyshakhgnair.cvr@gmail.com>
2022-03-22 10:41:23 +0530

    added user table

--- model.py
+++ model.py
@@ -1,29 +1,20 @@
 
-from flask import Flask, request, redirect
-from flask.templating import render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
 app = Flask(__name__)
 app.debug = True
  
 # adding configuration for using a sqlite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
  
 # Creating an SQLAlchemy instance
 db = SQLAlchemy(app)
  
 # Models
-class User(db.Model, UserMixin):
+class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True,autoincrement=True,nullable=False)
-    user_name = db.Column(db.String(150),nullable=False)
+    user_name = db.Column(db.String(150),nullable=False,unique=True)
     email = db.Column(db.String(150), unique=True,nullable=False)
     password = db.Column(db.String(150),nullable=False)
  
-# function to render index page
-@app.route('/')
-def index():
-    return render_template('signup.html')
- 
-if __name__ == '__main__':
-    app.run()
